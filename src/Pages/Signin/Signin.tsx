9c4016838cc6ba785f889cf5a671261540a4eba8
prabhjet singh <prabhjeet127.s@gmail.com>
2026-02-23 13:10:42 +0530

    fixing

--- src/Pages/Signin/Signin.tsx
+++ src/Pages/Signin/Signin.tsx
@@ -44,8 +44,7 @@ const Signin = () => {
                 <img src='/loginMain.jpg' className='  object-cover w-full  h-full' alt="none" />
             </div>
 
-
-            <div className='   rounded-4xl h-full  pt-15 w-[40%] ' >  {/*right box*/}
+            <div className='rounded-4xl h-full  pt-15 w-[40%] ' >  {/*right box*/}
 
                 <div className='  flex  justify-center w-full  ' >
                     <div className='flex flex-col  items-center w-auto    font-semibold mt-10 p-14  ' >
@@ -60,8 +59,8 @@ const Signin = () => {
                                     <input value={email} onChange={(e) => setemail(e.target.value)} type="email" className='border-gray-200 bg-blue-100 border-4  p-2 rounded-2xl ' name="" placeholder='Enter your email ' id="" />
                                 </div>
                                 <div className='flex flex-col w-lg  gap-2 p-2 ' >
-                                    <label htmlFor="Password"       >Password</label>
-                                    <input value={password} onChange={(e) => setpassword(e.target.value)} type="Password" className='border-gray-200  bg-blue-100  text-2xl border-4  p-2 rounded-2xl ' name="" placeholder='Enter your password ' id="" />
+                                    <label htmlFor="Password">Password</label>
+                                    <input value={password} onChange={(e) => setpassword(e.target.value)} type="Password" className='border-gray-200 bg-blue-100 border-4  p-2 rounded-2xl  ' name="" placeholder='Enter your password ' id="" />
                                 </div>
                                 <div className=' rounded-xl   w-full m-6 text-center p-2' >
                                     <button className='text-white  w-70 rounded-lg p-2 text-xl shadow-2xl bg-red-700  hover:opacity-85 '  >Login</button>
